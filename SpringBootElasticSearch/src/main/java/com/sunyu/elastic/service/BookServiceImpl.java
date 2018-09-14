a1a1796f71898d676a59fb84bec20b6efd0c7e8d
oppofind <836575280@qq.com>
2018-09-14 15:25:04 +0800

    修改BookServiceImpl

--- SpringBootElasticSearch/src/main/java/com/sunyu/elastic/service/BookServiceImpl.java
+++ SpringBootElasticSearch/src/main/java/com/sunyu/elastic/service/BookServiceImpl.java
@@ -16,12 +16,8 @@ import java.util.List;
 @Service
 public class BookServiceImpl implements BookService {
 
-    private BookRepository bookRepository;
-
     @Autowired
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
+    private BookRepository bookRepository;
 
 
     public Book save(Book book) {
